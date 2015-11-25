d054848763731ba9363a5eee5aaa0073ce3a450b
Riccardo Gargano <riccardo@MacBook-Pro-di-Riccardo.local>
2015-11-25 11:37:11 +0100

    Initial Commit

---
+++ TextFix/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TextFix
+//
+//  Created by Riccardo Gargano on 25/11/15.
+//  Copyright (c) 2015 Riccardo Gargano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TextFix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TextFix
+//
+//  Created by Riccardo Gargano on 25/11/15.
+//  Copyright (c) 2015 Riccardo Gargano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
